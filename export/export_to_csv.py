e66a09f91cf97bc509927cbe7a4ccfea7c980fab
OB-W <oliverwalters01@gmail.com>
2026-05-01 20:19:54 +0100

    bug fixes and code improvements

--- export/export_to_csv.py
+++ export/export_to_csv.py
@@ -5,17 +5,17 @@ def export_data(tracks, file='output/data.csv'): # saving to csv file, track has
     rows = []
 
     for frame_num, players in enumerate(tracks["players"]): # goes through each frame and players in tracks["players"]
-        ball = tracks["ball"][frame_num]
+        ball = tracks["ball"][frame_num] #
 
-        if 1 in ball:
+        if 1 in ball: #
             ball_x, ball_y = get_center_of_bbox(ball[1]["bbox"])
-        else:
+        else: # 
             ball_x, ball_y = False, False
 
         for player_id, player in players.items(): # goes through each player in the frame
-            x, y = player["position"]
+            x, y = player["position"] #
             team = player.get("team", None) # won't crash if not their
-            has_ball = player.get("has_ball", False)
+            has_ball = player.get("has_ball", False) #
 
             rows.append({   ## addind row for each player in frame with the, player id, team, player position, ball position, has_ball
                 "frame":frame_num,
@@ -29,8 +29,38 @@ def export_data(tracks, file='output/data.csv'): # saving to csv file, track has
             })
 
     dataframe = pd.DataFrame(rows) 
+    dataframe = pass_validation(dataframe)
     dataframe.to_csv(file, index=False) ## saving data to csv
    # print("saved file") #check this only temp
+
+def pass_validation(dataframe): ## maybe refractor could be gold plating ref
+    frame_count = {}
+    hold_count = {}
+    for frame, rows in dataframe.groupby('frame'):
+        holder = None
+        for _, row in rows.iterrows():
+            if row['has_ball'] == True:
+                holder = row['player_id']
+    
+        if holder is None: 
+            dataframe.loc[dataframe['frame'] == frame, 'has_ball'] = False
+            continue
+        
+        hold_count = {holder: hold_count.get(holder, 0) + 1}
+    
+        if holder in hold_count:
+            hold_count[holder] += 1
+        else:
+            hold_count[holder] = 1 
+    
+        if hold_count[holder] >= 25:
+            dataframe.loc[(dataframe['frame'] == frame) & (dataframe['player_id'] == holder), 'has_ball'] = True
+            dataframe.loc[(dataframe['frame'] == frame) & (dataframe['player_id'] != holder), 'has_ball'] = False
+        else:
+            dataframe.loc[dataframe['frame'] == frame, 'has_ball'] = False  
+    dataframe['has_ball'] = dataframe['has_ball'].astype(bool)    
+    return dataframe
+
 
 
 
---
+++ pass_data_processing/data_processing.py
@@ -0,0 +1,74 @@
+import pandas as pd
+import os
+import numpy
+import math
+
+path = '/home/c3646202/Desktop/FootballPassingAnalysisProject2/output/data.csv' # csv
+# path = 'output/data.csv'
+
+#https://pandas.pydata.org/docs/index.html
+
+def csv_import():
+    data = pd.read_csv(path)
+    print(data.head())
+    return data
+
+def import_ML_Data(data, passes_dataframe): ## run this just before you create passes.csv
+    for i in range(len(data)):
+        passes_dataframe['pass_grade'] = data{i}
+    return passes_dataframe
+
+
+
+
+
+def detect_passes(dataframe): #check
+    passes = []
+    last_holder = None
+   # pass_grade = None 
+    #current_holder = None
+  #  pass_grade = None
+    start_frame = None ## will not work if we actually start from a certain frame
+    i = 0
+    # for frame in dataframe["frame"].unique(): ## looing through the frames and 
+    for frame, rows in dataframe.groupby('frame'): ## looing through the frames and 
+       # rows = dataframe[dataframe['frame'] == frame]
+        holders = rows[rows["has_ball"] == 1] 
+        current_holder = holders['player_id'].values[0] if len(holders) > 0 else None
+        if len(holders) == 0:
+            continue
+        current_holders = holders['player_id'].values[0]
+        
+        if last_holder is None:
+            last_holder = current_holder
+            start_frame = frame
+            continue
+
+        if current_holder != last_holder:  ### will be equal if there is no 
+            
+            pass_grade = holders['pass_grade'].iloc[0] if 'pass_grade' in holders.columns else None
+            passes.append({'passes_id': i, 'player_id' : last_holder, 'reciever_id': current_holder, 
+            'start_frame':start_frame, 'end_frame':  frame, 'pass_grade': pass_grade}) ## will be na if there are no holder then frame will just always fill 
+            i +=1
+            last_holder = current_holder
+            start_frame= frame
+        else:
+            if start_frame is None:
+                start_frame = frame
+            # this need to be cleaned - below or recfaotro and the main as well
+    passes_dataframe = pd.DataFrame(passes) ## ?
+## duration
+    passes_dataframe = import_ML_Data(data, passes_dataframe)
+    passes_dataframe.to_csv('passes.csv', index=False)
+    print('detect_passes finsihed')
+    return passes_dataframe
+
+
+
+
+def main():
+    dataframe = csv_import()
+    passes = detect_passes(dataframe)
+
+main()
+
